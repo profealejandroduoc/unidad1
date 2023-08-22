7213b26d5735c25a2cecb4d717d8823b24e1a3ae
El profe <al.sepulvedam@profesor.duoc.cl>
2023-08-22 09:44:04 -0400

    Prop titulo agregado

--- src/app/components/encabezado/encabezado.component.ts
+++ src/app/components/encabezado/encabezado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-encabezado',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncabezadoComponent  implements OnInit {
 
+  @Input() titulo='';
   constructor() { }
 
   ngOnInit() {}
